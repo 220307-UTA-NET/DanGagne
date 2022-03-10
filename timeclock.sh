dec4154e94ccd672bd850f08d9c74c01761c02a0
Revanescence <jdangagne@gmail.com>
2022-03-10 16:34:50 -0500

    updated timeclock.sh

---
+++ timeclock.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash/
+
+echo "Running timeclock.sh..."
+ClockIns=()
+IFS=""
+while [ true ]; do
+
+	read -p $'Enter your name here:\n' input
+	timestamp=$(date +"%Y-%m-%d %T")
+
+
+	if [[ ${input^^} == "STOP" ]]; then
+		break
+	elif [[ -z $input ]]; then
+		echo $'Empty input. \nPlease enter a valid name.'
+	else
+		sleep 1
+		echo $'\n'$input
+		echo "Clocked in: "$timestamp$'\n'
+		combine=$"$input: $timestamp"
+		ClockIns+=($combine)
+	fi
+done
+
+for name in ${ClockIns[@]}; do
+	echo $name >> ./ClockInOut.txt
+done
+
+echo "Exiting Timeclock"
